b3eccdb6f715540fa60ce6190d8ff645c2e47c5f
Null-Austin <133458852+Null-Austin@users.noreply.github.com>
2025-05-23 14:31:22 -0500

    updated to include inline files!+

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 // configs for future
 const path = require('path')
+const fs = require('fs')
 
 // config express
 const express = require('express')
@@ -8,8 +9,29 @@ const port = 3000;
 app.set('views', path.join(__dirname, '../src/backend_assets/web'));
 app.set('view engine', 'ejs');
 
+//ejs functions
+app.locals.inlineFile = function(filepath) {
+  try {
+    const fullPath = path.join(__dirname, 'backend_assets/web', filepath);
+
+    if (!fs.existsSync(fullPath) || fs.statSync(fullPath).isDirectory()) {
+      console.error(`[inlineFile] "${fullPath}" is not a valid file.`);
+      return '';
+    }
+
+    return fs.readFileSync(fullPath, 'utf8');
+  } catch (err) {
+    console.error(`Failed to load from ${filepath}:`, err);
+    return '';
+  }
+};
+
 app.get('/',(req,res,next)=>{
-    res.render('index.ejs')
+    res.redirect('/home')
+})
+
+app.get('/home',(req,res,next)=>{
+    res.render('pages/index.ejs')
 })
 
 app.listen(port,err=>{

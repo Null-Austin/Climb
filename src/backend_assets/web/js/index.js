bb75fdf3244186b203b3d80487d3bde65c3da0b7
Null-Austin <133458852+Null-Austin@users.noreply.github.com>
2025-05-24 11:50:17 -0500

    fixed clicking the photo

--- src/backend_assets/web/js/index.js
+++ src/backend_assets/web/js/index.js
@@ -13,10 +13,10 @@ document.addEventListener('DOMContentLoaded',async (e)=>{
         clone.querySelector('.user').innerText = post['username']
         document.querySelector('div#content').appendChild(clone)
 
-        clone.addEventListener('click', (e) => {
-            //create a new tab with the image url
-            var img = clone = clone.querySelector('img').src
-            var win = window.open(img, '_blank');
-        })
+        clone.querySelector('img.post-image').addEventListener('click', () => {
+            // Open the image in a new tab on every click
+            const imgSrc = clone.querySelector('img').src;
+            window.open(imgSrc, '_blank');
+        });
     })
 })

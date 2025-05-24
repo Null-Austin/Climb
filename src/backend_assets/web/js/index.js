8e568ececfc83c9f5f97d6bfffc6ebfe62cf6bc1
Null-Austin <133458852+Null-Austin@users.noreply.github.com>
2025-05-24 00:25:48 -0500

    added post template + js automation

--- src/backend_assets/web/js/index.js
+++ src/backend_assets/web/js/index.js
@@ -1,5 +1,16 @@
 document.addEventListener('DOMContentLoaded',async (e)=>{
     var data = await fetch('/api/fyp-get')
     var json = await data.json()
+    json = json['data']['posts']
     console.log(json)
+
+    const template = document.getElementById('post-template')
+    json.forEach(post=>{
+        var clone = template.cloneNode(true)
+        clone.id=''
+        clone.querySelector('span.details').innerText = post['content']
+        clone.querySelector('img').src = post['image']
+        clone.querySelector('.user').innerText = post['user']
+        document.querySelector('div#content').appendChild(clone)
+    })
 })

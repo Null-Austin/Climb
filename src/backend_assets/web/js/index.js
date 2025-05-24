00b522d96cf9fda74809ff9319cdc25e418c7b24
Null-Austin <133458852+Null-Austin@users.noreply.github.com>
2025-05-24 11:04:10 -0500

    better styling! + more img support

--- src/backend_assets/web/js/index.js
+++ src/backend_assets/web/js/index.js
@@ -12,5 +12,11 @@ document.addEventListener('DOMContentLoaded',async (e)=>{
         clone.querySelector('img').src = post['image']
         clone.querySelector('.user').innerText = post['username']
         document.querySelector('div#content').appendChild(clone)
+
+        clone.addEventListener('click', (e) => {
+            //create a new tab with the image url
+            var img = clone = clone.querySelector('img').src
+            var win = window.open(img, '_blank');
+        })
     })
 })

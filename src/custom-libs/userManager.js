54720546367cdb6f93d193d50c9fc014b847e3db
Null-Austin <133458852+Null-Austin@users.noreply.github.com>
2025-05-24 21:09:47 -0500

    added crypto dependency and implemented user session token generation

--- src/custom-libs/userManager.js
+++ src/custom-libs/userManager.js
@@ -5,9 +5,14 @@ todo
 [ ] - login/create session creator
  */
 const sqlite3 = require('sqlite3')
+const crypto = require('crypto')
+
 class userManager{
     constructor(db){
         this.db = new sqlite3.Database(db)
+    }
+    generateUserToken() {
+        return crypto.randomBytes(128).toString('hex') // 128 bytes * 2 hex chars = 256 chars
     }
     getUserFromId(id){
         return new Promise((resolve, reject) => {
@@ -31,6 +36,46 @@ class userManager{
                 }
             });
         });
+    }
+    async loginToSession(username, password, changeSession = true) {
+        // Helper function to get user details from the database
+        const getDetails = (username, password) => {
+            return new Promise((resolve, reject) => {
+                this.db.get(
+                    'SELECT * FROM users WHERE username LIKE ? AND password = ?',
+                    [username, password],
+                    (err, row) => {
+                        if (err) {
+                            reject(err);
+                        } else {
+                            resolve(row);
+                        }
+                    }
+                );
+            });
+        };
+
+        if (changeSession) {
+            return new Promise((resolve, reject) => {
+                this.db.run(
+                    'UPDATE users SET session = ? WHERE username LIKE ? AND password = ?',
+                    [this.generateUserToken(), username,password],
+                    function (err) {
+                        if (err) {
+                            reject(err);
+                        } else {
+                            // Optionally get the updated user details after session update
+                            getDetails(username, password)
+                                .then(resolve)
+                                .catch(reject);
+                        }
+                    }
+                );
+            });
+        } else {
+            // Just return the user details without changing the session
+            return getDetails(username, password);
+        }
     }
 }
 module.exports = userManager;

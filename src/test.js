6a84a8d8863df68a7748c161f784e47f572a8fdb
Null-Austin <133458852+Null-Austin@users.noreply.github.com>
2025-05-24 21:10:21 -0500

    modified test

--- src/test.js
+++ src/test.js
@@ -4,7 +4,8 @@ const userManager = require('./custom-libs/userManager')
 const users = new userManager(path.join(__dirname,'database','users','users.db'))
 
 async function run(){
-    let user = await users.getUserFromSession('acd')
+    console.log('ran')
+    let user = await users.loginToSession('Austin','admin',false)
     console.log(user)
 }
 run()

2abfdb24d6f9428a3c9b05145fb2eb7dcc1367ef
Null-Austin <133458852+Null-Austin@users.noreply.github.com>
2025-05-26 05:02:45 -0500

    fix event listener for DOMContentLoaded in user.js

--- src/backend_assets/web/js/auth/user.js
+++ src/backend_assets/web/js/auth/user.js
@@ -1,6 +1,6 @@
 const page = window.location.pathname;
 
-window.addEventListener('DOMContentLoaded',e=>{
+window.addEventListener('DOMContentLoaded',()=>{
     let login = document.querySelector('#login');
     let signup = document.querySelector('#register');
 

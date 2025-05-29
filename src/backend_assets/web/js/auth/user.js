1bfe29208bbd9801c018d4972729c43ec1278180
Null-Austin <133458852+Null-Austin@users.noreply.github.com>
2025-05-29 02:21:15 -0500

    add signup form action and improve navigation between login and signup pages

--- src/backend_assets/web/js/auth/user.js
+++ src/backend_assets/web/js/auth/user.js
@@ -1,9 +1,23 @@
-const page = window.location.pathname;
-
+let page = window.location.pathname;
+if (page.endsWith('login')){
+    page = 'login';
+} else if (page.endsWith('signup')){
+    page = 'signup';
+}
 window.addEventListener('DOMContentLoaded',e=>{ /* webstorm, for the love of god, i will use e and not (), if i have 500 differnet listeners thats 500 saved chars. or almost 4000 bits :O*/
     let login = document.querySelector('#login');
     let signup = document.querySelector('#register');
 
     login.disabled = false
     signup.disabled = false
+
+    if (page === 'login'){
+        signup.addEventListener('click',()=>{
+            window.location.href='/auth/signup'
+        })
+    } else if (page === 'signup'){
+        login.addEventListener('click',()=>{
+            window.location.href='/auth/login'
+        })
+    }
 })
--- src/custom-libs/userManager.js
+++ src/custom-libs/userManager.js
@@ -1,11 +1,11 @@
 /*
 todo
-[ ] - create account system
-[ ] - verify email system, with resend
-[X] - login/create session creator
+[ ] - Hash password stuff
  */
 const sqlite3 = require('sqlite3')
 const crypto = require('crypto')
+const emailverificator = require('./email-verificator')
+const verificator = new emailverificator()
 
 class userManager{
     constructor(db){
@@ -13,6 +13,28 @@ class userManager{
     }
     generateUserToken() {
         return crypto.randomBytes(128).toString('hex') // 128 bytes * 2 hex chars = 256 chars
+    }
+    getUserFromUsername(username){
+        return new Promise((resolve, reject) => {
+            this.db.get('SELECT * FROM users WHERE username = ?', [username], (err, row) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(row);
+                }
+            });
+        });
+    }
+    getUserFromEmail(email){
+        return new Promise((resolve, reject) => {
+            this.db.get('SELECT * FROM users WHERE email = ?', [email], (err, row) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(row);
+                }
+            });
+        });
     }
     getUserFromId(id){
         return new Promise((resolve, reject) => {
@@ -76,6 +98,25 @@ class userManager{
             // Just return the user details without changing the session
             return getDetails(username, password);
         }
+    }
+    async createUser(role=0,username,password,email=false,replace=false){
+        if (await this.getUserFromUsername(username) && !replace){
+            throw new Error('Username already exists');
+        }
+        return new Promise((resolve, reject)=>{
+            this.db.run('INSERT INTO users (username, password,role,email) VALUES (?,?,?,?)',[username,password,role,email],(e)=>{
+                if(e){
+                    reject(e);
+                } else{
+                    resolve(this.getUserFromUsername(username));
+                }
+            })
+        })
+    }
+    async updateUser(x){
+        let userId = x.id;
+        // Update the user in the db, with whatever is in the x var, but track the user using the id.
+        
     }
 }
 module.exports = userManager;
